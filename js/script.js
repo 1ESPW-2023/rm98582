7b06616dcd756612b7b52bd4c31d204b009c632c
Akiozin <rm98582@fiap.com.br>
2023-04-11 21:24:15 -0300

    .

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//Exemplo de funções
+const divElement = document.getElementById("div-box")
+
+divElement.addEventListener("click", mostraElementos);
+
+function mostraElementos() {
+    console.log(divElement)
+}
+
+function some () {
+    let n1 = 10
+    let n2 = 5
+    console.log(n1 + n2)
+}
