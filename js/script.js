ee551bfebe571e0315ce4318279889df47093e33
Akiozin <rm98582@fiap.com.br>
2023-05-02 21:35:50 -0300

    opção login adiocinado

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ function mudaCor() {
 }
 
 function alteraBanner1() {
-
+    
     // let nr = Math.ceil(Math.random() *3);
     const img1 = document.querySelector(".l-d > img");
     const img2 = document.querySelector(".l-e > img");
@@ -66,9 +66,9 @@ function alteraBanner1() {
     setTimeout(alteraBanner2, 1000);
 
 }
-
+ 
 function alteraBanner2() {
-
+    
     // let nr = Math.ceil(Math.random() *3);
     const img1 = document.querySelector(".l-d > img");
     const img2 = document.querySelector(".l-e > img");
@@ -77,10 +77,10 @@ function alteraBanner2() {
 
     setTimeout(alteraBanner3, 1000);
 
-} 
+}
 
 function alteraBanner3() {
-
+    
     // let nr = Math.ceil(Math.random() *3);
     const img1 = document.querySelector(".l-d > img");
     const img2 = document.querySelector(".l-e > img");
@@ -92,3 +92,29 @@ function alteraBanner3() {
 }
 
 alteraBanner1();
+
+const btnOnOff = document.querySelector(".conteudo button");
+
+btnOnOff.addEventListener("click", ()=>{
+    const imgLampada = document.querySelector(".conteudo img");
+    if(btnOnOff.textContent == "LIGAR"){
+        btnOnOff.textContent = "DESLIGAR";
+        imgLampada.src = "./img/pic_bulbon.gif";
+    }else{
+        btnOnOff.textContent = "LIGAR";
+        imgLampada.src = "./img/pic_bulboff.gif";
+    }
+});
+
+// function turnInOff() {
+//     const btnOnOff = document.querySelector(".conteudo button");
+
+//     if(btnOnOff.textContent == "LIGAR"){
+//         btnOnOff.textContent = "DESLIGAR";
+//         imgLampada.src = "./img/pic_bulbon.gif";
+//     }else{
+//         btnOnOff.textContent = "LIGAR";
+//         imgLampada.src = "./img/pic_bulboff.gif";
+//     }
+// }
+
